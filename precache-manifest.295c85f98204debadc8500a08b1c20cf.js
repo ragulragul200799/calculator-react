f59a8e75d5ac0201af6c8e7c2a6b7e454acf22d1
Ragul <ragulragul873@gmail.com>
2020-05-13 19:09:21 +0530

    Updates

---
+++ precache-manifest.295c85f98204debadc8500a08b1c20cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "904e67f04421a5ba8d1cb89af6bfcbcc",
+    "url": "/calculator-react/index.html"
+  },
+  {
+    "revision": "76b7c915b2ef5a970264",
+    "url": "/calculator-react/static/css/2.6475d966.chunk.css"
+  },
+  {
+    "revision": "8189002a397df63e18e9",
+    "url": "/calculator-react/static/css/main.3f68c979.chunk.css"
+  },
+  {
+    "revision": "76b7c915b2ef5a970264",
+    "url": "/calculator-react/static/js/2.3d932b27.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/calculator-react/static/js/2.3d932b27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8189002a397df63e18e9",
+    "url": "/calculator-react/static/js/main.a81160a3.chunk.js"
+  },
+  {
+    "revision": "9f5a50e9c8a4fc25c1c2",
+    "url": "/calculator-react/static/js/runtime-main.a09c319c.js"
+  }
+]);
