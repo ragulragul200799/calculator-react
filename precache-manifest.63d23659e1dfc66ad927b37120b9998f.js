6df22125ee89d5a1b1e6efe89ef700a50c249845
Ragul <ragulragul873@gmail.com>
2020-05-14 17:30:24 +0530

    Updates

---
+++ precache-manifest.63d23659e1dfc66ad927b37120b9998f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f4cf5d27c50934c1866202638927b22",
+    "url": "/calculator-react/index.html"
+  },
+  {
+    "revision": "76b7c915b2ef5a970264",
+    "url": "/calculator-react/static/css/2.6475d966.chunk.css"
+  },
+  {
+    "revision": "e2e51de513f13636a242",
+    "url": "/calculator-react/static/css/main.3f68c979.chunk.css"
+  },
+  {
+    "revision": "76b7c915b2ef5a970264",
+    "url": "/calculator-react/static/js/2.3d932b27.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/calculator-react/static/js/2.3d932b27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2e51de513f13636a242",
+    "url": "/calculator-react/static/js/main.1072099e.chunk.js"
+  },
+  {
+    "revision": "9f5a50e9c8a4fc25c1c2",
+    "url": "/calculator-react/static/js/runtime-main.a09c319c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/budgetcalculation/precache-manifest.3d64c790d124d5efad22e3d7ffd9d509.js"
+  "/calculator-react/precache-manifest.63d23659e1dfc66ad927b37120b9998f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/budgetcalculation/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/calculator-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
